f179584195d5423de365bb4ffc428aced8fd8a4f
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-24 02:04:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if between?(1,9)
-  if (board[index] == be_between && board[index] == "" || board[index]== " " || board[index] == nil)
+  if 
+  if board[index].between?(1,9) && board[index] == "" || board[index]== " " || board[index] == nil)
   true 
 end
 
